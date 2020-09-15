d6effc15cf5c69bd3348a12737996a086476e9f0
ROBINSON <Adam.ROBINSON@EDUCATION.GOV.UK>
2020-09-15 16:28:34 +0100

    Remove iconv

--- R/read.R
+++ R/read.R
@@ -13,7 +13,7 @@ read_layout <- function(file){
 
   file1 <- readLines(file, warn = FALSE, skipNul = TRUE)
 
-  file1 <- iconv(file1, "windows-1252", "UTF-8")
+  #file1 <- iconv(file1, "windows-1252", "UTF-8")
 
   RJSONIO::fromJSON(file1, digits = 15)
 
